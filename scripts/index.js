219e1b6a05b0190a2d3e88ad7cee44fa8fb3ab43
EstudioVilcayMaya <112893881+EstudioVilcayMaya@users.noreply.github.com>
2022-09-05 15:07:38 -0300

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+let buttonMenu = document.getElementById("button-menu");
+
+let backgroundBlur = document.getElementById("page-mask");
+
+let burguerMenu = document.getElementById("burguer-menu");
+
+let menuOpen = false;
+
+let click = 0;
+
+function toggleVisibility(element) {
+  element.style.visibility = "visible";
+  menuOpen = true;
+}
+
+function toggleOpacity(element) {
+  element.style.opacity = "1";
+  menuOpen = true;
+}
+
+buttonMenu.addEventListener("click", function openMenu() {
+  toggleVisibility(backgroundBlur);
+  toggleVisibility(burguerMenu);
+  toggleOpacity(backgroundBlur);
+  toggleOpacity(burguerMenu);
+  closeMenu();
+});
+
+let eventHander = function (event) {
+  const withinBoundaries = event.composedPath().includes(burguerMenu);
+  if (!withinBoundaries) {
+    click++;
+    if (click > 1) {
+      backgroundBlur.style.visibility = "hidden";
+      burguerMenu.style.visibility = "hidden";
+      backgroundBlur.style.opacity = "0";
+      burguerMenu.style.opacity = "0";
+      click = 0;
+    }
+  }
+};
+
+function closeMenu() {
+  if (menuOpen) {
+    document.addEventListener("click", eventHander);
+  } else if (!menuOpen) {
+    document.removeEventListener("click", eventHander);
+    click = 0;
+  }
+  click = 0;
+}
